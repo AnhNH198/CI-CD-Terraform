0aa045617b6a4c17a29665b13fd5772480573b21
boploi <boploi198@gmail.com>
2021-03-29 16:48:37 +0700

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,13 +4,11 @@ provider "azurerm" {
   }
 }
 
-
-
 terraform {
     backend "azurerm" {
       
     }
-  required_providers {
+    required_providers {
     azurerm = {
         source = "hashicorp/azurerm"
         version = "=2.20.0"
